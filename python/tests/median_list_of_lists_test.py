9ab478d34256d422c6559d0e4231193bd66b456a
Damien Menanteau <damien.menanteau@gmail.com>
2023-04-05 12:06:35 +0200

    Change python test framework from unittest to pytest

--- python/tests/median_list_of_lists_test.py
+++ python/tests/median_list_of_lists_test.py
@@ -1,32 +1,42 @@
-import unittest
-
-from parameterized import parameterized
+import pytest
+from pytest import approx
+from parametrization import Parametrization
 from median_list_of_lists.median_list_of_lists import compute_median
 
 
-class MedianListOfListsTest(unittest.TestCase):
+class TestListOfLists:
 
-    @parameterized.expand([
-        ("with odd number of values", [
+    @pytest.mark.skip(reason="test currently disabled")  # Comment or remove this line to enable this test case
+    @Parametrization.autodetect_parameters()
+    @Parametrization.case(
+        name="with odd number of values",
+        values=[
             [1, 42, 7, 49, 18, 97],
             [24, 1, 12, 82, 33, 40, 73, 56, 57],
             [46, 68, 2, 72],
-        ], 42, 0),
-        ("with even number of values", [
+        ],
+        expected=42,
+        delta=0,
+    )
+    @Parametrization.case(
+        name="with even number of values",
+        values=[
             [1, 7, 49, 18, 97],
             [24, 1, 12, 82, 33, 41, 73, 56, 57],
             [43, 68, 2, 72],
-        ], 42, 0),
-        ("with non-integer median", [
+        ],
+        expected=42,
+        delta=0,
+    )
+    @Parametrization.case(
+        name="with non-integer median",
+        values=[
             [1, 7, 49, 18, 97],
             [24, 1, 12, 82, 33, 41, 73, 56, 57],
             [44, 68, 2, 72],
-        ], 42.5, 0),
-    ])
-    @unittest.skip("test currently disabled")  # Comment or remove this line to enable this test case
-    def test_acceptance_tests(self, _name, values, expected, delta):
-        self.assertAlmostEqual(expected, compute_median(values), None, None, delta)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        ],
+        expected=42.5,
+        delta=0,
+    )
+    def test_acceptance_tests(self, values, expected, delta) -> None:
+        assert (compute_median(values) == approx(expected, abs=delta))
