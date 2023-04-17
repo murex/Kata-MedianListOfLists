9bc184a543634db80b256e533f6d48cae112fbbd
Damien Menanteau <damien.menanteau@gmail.com>
2023-04-17 17:59:55 +0200

    Upgrade java bootstraps to junit 5

--- java/src/test/java/com/murex/MedianListOfListsTest.java
+++ java/src/test/java/com/murex/MedianListOfListsTest.java
@@ -22,43 +22,43 @@ SOFTWARE.
 
 package com.murex;
 
-import org.junit.Ignore;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
 import static java.util.Arrays.asList;
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 public class MedianListOfListsTest {
-
-    @Ignore
+    
+    @Disabled
     @Test
     public void acceptance_test_with_odd_number_of_values() {
         List<List<Integer>> values = asList(
-                asList(1, 42, 7, 49, 18, 97),
-                asList(24, 1, 12, 82, 33, 40, 73, 56, 57),
-                asList(46, 68, 2, 72));
+            asList(1, 42, 7, 49, 18, 97),
+            asList(24, 1, 12, 82, 33, 40, 73, 56, 57),
+            asList(46, 68, 2, 72));
         assertEquals(42, MedianListOfLists.computeMedian(values), 0);
     }
-
-    @Ignore
+    
+    @Disabled
     @Test
     public void acceptance_test_with_even_number_of_values() {
         List<List<Integer>> values = asList(
-                asList(1, 7, 49, 18, 97),
-                asList(24, 1, 12, 82, 33, 41, 73, 56, 57),
-                asList(43, 68, 2, 72));
+            asList(1, 7, 49, 18, 97),
+            asList(24, 1, 12, 82, 33, 41, 73, 56, 57),
+            asList(43, 68, 2, 72));
         assertEquals(42, MedianListOfLists.computeMedian(values), 0);
     }
-
-    @Ignore
+    
+    @Disabled
     @Test
     public void acceptance_test_with_non_integer_median() {
         List<List<Integer>> values = asList(
-                asList(1, 7, 49, 18, 97),
-                asList(24, 1, 12, 82, 33, 41, 73, 56, 57),
-                asList(44, 68, 2, 72));
+            asList(1, 7, 49, 18, 97),
+            asList(24, 1, 12, 82, 33, 41, 73, 56, 57),
+            asList(44, 68, 2, 72));
         assertEquals(42.5, MedianListOfLists.computeMedian(values), 0);
     }
 }
